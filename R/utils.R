01eb4bd6444c55eadbe58ae8088cdd2c21071e4e
Brett Stacy <bstacy2@uw.edu>
2024-07-17 05:49:01 -0700

    comp_sample2 works. now merge into boot_length and delete.

--- R/utils.R
+++ R/utils.R
@@ -11,9 +11,14 @@ comp_sample2 = function(DT) {
   DT %>%
     tidytable::uncount(SUM_FREQUENCY) %>%
     tidytable::mutate(LENGTH = comp_sample(LENGTH, n.samples = .N), .by = c(YEAR, hauljoin_unique)) %>%
-    tidytable::summarise(SUM_FREQUENCY = n(unique(LENGTH)), .by = c(YEAR, hauljoin_unique, LENGTH)) %>%
-    tidytable::left_join(DT) %>%
-    tidytable::drop_na() # LEFT OFF HERE 7/16 1:54.
+    tidytable::summarise(SUM_FREQUENCY = n(unique(LENGTH)), .by = c(YEAR, hauljoin_unique, LENGTH)) -> .resampled_lengths
+    # tidytable::left_join(DT) %>%
+    # tidytable::drop_na() # LEFT OFF HERE 7/16 1:54.
+
+  DT %>%
+    tidytable::distinct(YEAR, hauljoin_unique, .keep_all = T) %>%
+    tidytable::select(-LENGTH, -SUM_FREQUENCY) %>%
+    tidytable::right_join(.resampled_lengths)
 
 
 }
