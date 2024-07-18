e246b9ed6eeab6c5ad156af9df6744c2109e33d7
Brett Stacy <bstacy2@uw.edu>
2024-07-18 15:52:50 -0700

    done first draft, works in compute_stats

--- R/bias_length.R
+++ R/bias_length.R
@@ -1,13 +1,22 @@
-#' calculate bias in bootstrapped length composition
+#' calculate bias in bootstrapped proportions-at-length
 #'
 #' @description
 #' Computes the mean bias in bootstrapped population at length compared to original unsampled
 #' population at length
 #'
-#' @param r_length iterated population at length
-#' @param ogl original population at length without any sampling
+#' @param sim_length_props list of replicated abundance at length
+#' @param og_length_props original abundance at length (computed with data that has not been resampled)
 #'
 #' @export
 #'
-bias_length <- function(r_length,
-                        ogl) {
+bias_length <- function(sim_length_props,
+                        og_length_props) {
+
+
+  sim_length_props %>%
+    tidytable::left_join(og_length_props) %>%
+    tidytable::mutate(bias = sim_FREQ - og_FREQ) %>%
+    tidytable::summarise(bias = base::mean(bias), .by = YEAR)
+
+
+}
