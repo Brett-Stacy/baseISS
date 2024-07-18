fba3e1dcbb664d159f36a160eee86a266b57c106
Brett Stacy <bstacy2@uw.edu>
2024-07-18 14:39:56 -0700

    start iss_length function

---
+++ R/iss_length.R
@@ -0,0 +1,17 @@
+#' Calculate length composition input sampled size (iss)
+#'
+#' @description
+#' Function to calculate length composition iss (harmonic mean of realized sample sizes) and add nominal sample
+#' size (the number of lengths actually measured) and the number of sampled hauls to output.
+#'
+#' @param rss_length iterated length composition realized sample size
+#' @param lfreq_data length frequency input dataframe
+#'
+#' @export
+#'
+iss_length <- function(rss_length,
+                       lfreq_data) {
+
+
+
+}
