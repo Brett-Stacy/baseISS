a0e069f86b23bb4918b4fda457b390ee26d33be1
Brett Stacy <bstacy2@uw.edu>
2024-07-18 15:54:22 -0700

    don first draft, works in fishery_iss

--- R/compute_stats.R
+++ R/compute_stats.R
@@ -8,12 +8,12 @@
 #' @param lfreq_data length frequency input dataframe
 #'
 #' @return list of dataframes for realized sample size by replicate (.rss_length for length composition),
-#' input sample size BY YEAR? (.iss_length for length composition),
+#' input sample size by year? (.iss_length for length composition),
 #' bias in resampled comp data compared to original values (.bias_length for length composition)
 #'
 #' @export
 #'
-comp_stats <- function(sim_length_props,
+compute_stats <- function(sim_length_props,
                        og_length_props,
                        lfreq_data){
 
@@ -27,11 +27,10 @@ comp_stats <- function(sim_length_props,
   .iss_length <- iss_length(.rss_length, lfreq_data)
 
   # compute average relative bias in pop'n estimates (avg relative bias across length)
-  .bias_length <- bias_length(r_length, ogl)
+  .bias_length <- bias_length(sim_length_props, og_length_props)
 
   # return
   list(rss_length = .rss_length,
-       mean_length = .mean_length,
        iss_length = .iss_length,
        bias_length = .bias_length)
 
