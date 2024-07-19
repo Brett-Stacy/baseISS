098b02f02914885b72fe8796f1674757b288a315
Brett Stacy <bstacy2@uw.edu>
2024-07-19 11:40:37 -0700

    first attempt at furrr - doesn't work yet

--- R/fishery_iss.R
+++ R/fishery_iss.R
@@ -38,6 +38,22 @@ fishery_iss <- function(iters = 1,
                                          yrs = yrs,
                                          boot_thl = boot_thl))
 
+  # run resampling iterations with furrr ----
+  # not working yet. some error in passing function arguements up the chain.
+  # future::plan("multisession", workers = 14) # CHANGE THIS TO SUIT YOUR MACHINE AND NEEDS
+  # base::options(future.globals.maxSize= 2e9)
+  # rr <- furrr::future_map(1:iters, ~fishery_length_props(lfreq_data = lfreq_data,
+  #                                                 yrs = yrs,
+  #                                                 boot_thl = boot_thl),
+  #                         .options = furrr_options(seed = TRUE))
+  # future::plan("sequential")
+
+
+
+
+
+
+
   sim_length_props <- base::do.call(mapply, c(base::list, rr, SIMPLIFY = FALSE))$length %>% # reverse iterations[[type]] to type[[iterations]], i.e., to length[[1]]
     tidytable::map_df(~base::as.data.frame(.x), .id = "sim") %>% # over all iterations (list elements), map the data frame function to reduce the list to one combined data frame, creating a new column, .id = "sim". tidytable::map_df forces a tidytable class to result. The return is one long table of length = years x iterations x #lengthbins
     tidytable::rename(sim_FREQ = FREQ) -> .sim_length_props
--- R/run_the_thing.R
+++ R/run_the_thing.R
@@ -1,8 +1,13 @@
 iters = 10
 lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/y2_ebs_pcod_Brett.RDS")
-yrs = NULL
+yrs = 2015
 boot_thl = TRUE
+
+
+tictoc::tic()
 fishery_iss(iters = iters,
             lfreq_data = lfreq_data,
             yrs = yrs,
             boot_thl = boot_thl)
+tictoc::toc()
+
