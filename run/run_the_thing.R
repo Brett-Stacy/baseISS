4c3010225bcbe81dab578649ff2019d49a7b98b1
Brett Stacy <bstacy2@uw.edu>
2024-07-23 13:54:24 -0700

    run with TRIP resampled. works. 100 iters takes 10 minutes.

--- run/run_the_thing.R
+++ run/run_the_thing.R
@@ -1,5 +1,6 @@
-iters = 1
-lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/y2_ebs_pcod_Brett.RDS")
+iters = 100
+# lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/y2_ebs_pcod_Brett.RDS")
+lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/y2_ebs_pcod_Brett_TRIP.RDS")
 yrs = NULL
 boot_thl = TRUE
 
