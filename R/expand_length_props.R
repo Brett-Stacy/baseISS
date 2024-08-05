b51c727dcdd4d22c1ad2a5320834484918298610
Brett Stacy <bstacy2@uw.edu>
2024-08-05 10:49:25 -0700

    initiate switch to not expand using weighting factors 2 and 4. i.e., to test impact of weighitng on ISS

--- R/expand_length_props.R
+++ R/expand_length_props.R
@@ -5,11 +5,13 @@
 #'
 #' @param length_DT data table of length frequency data in the form of EBS Pcod y2 object
 #' @param expand_by_sampling_strata expand by observer sampling strata? If TRUE, then an additional weighting factor is calculated and applied to WEIGHT1 based on the number of fish caught in each sampling stratum.
+#' @param expand_by_weighting_factors expand using weighting factors? If TRUE, then then "WEIGHT2" and "WEIGHT4" are applied.
 #'
 #' @export
 #'
 expand_length_props = function(length_DT,
-                               expand_by_sampling_strata = FALSE) {
+                               expand_by_sampling_strata = FALSE,
+                               expand_using_weighting_factors = TRUE) {
 
 
   # Add in sampling strata weighting functionality ----
