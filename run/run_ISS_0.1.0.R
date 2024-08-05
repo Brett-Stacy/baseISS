51914b1b7141ca6d9d56e07f1a5a0cec2befa7d3
Brett Stacy <bstacy2@uw.edu>
2024-08-05 11:50:32 -0700

    run ISS without expansion. plot comparison in rmd

--- run/run_ISS_0.1.0.R
+++ run/run_ISS_0.1.0.R
@@ -1,10 +1,9 @@
-iters = 1000
-# lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/y2_ebs_pcod_Brett.RDS")
-# lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/y2_ebs_pcod_Brett_TRIP.RDS")
+iters = 10
 lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/baseISS_data/inputs/y2_ebs_pcod_Brett_TRIP_STRATA2.RDS")
 yrs = NULL
 boot_thl = TRUE
 expand_by_sampling_strata = TRUE
+expand_using_weighting_factors = FALSE
 
 
 tictoc::tic()
@@ -12,10 +11,11 @@ fishery_iss(iters = iters,
             lfreq_data = lfreq_data,
             yrs = yrs,
             boot_thl = boot_thl,
-            expand_by_sampling_strata = expand_by_sampling_strata) -> .output
+            expand_by_sampling_strata = expand_by_sampling_strata,
+            expand_using_weighting_factors = expand_using_weighting_factors) -> .output
 tictoc::toc()
 
 
-saveRDS(.output, file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/ISS_1000iters_output_8_2_24.RDS")
+saveRDS(.output, file = paste0("C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/baseISS_data/outputs/ISS_", iters, "iters_output_", Sys.Date(), ".RDS"))
 
 
