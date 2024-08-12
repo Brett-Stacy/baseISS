30f909aff99399c3ec41093e97701173e999cd5a
Brett Stacy <bstacy2@uw.edu>
2024-08-12 10:37:46 -0700

    set the initial stage for separate boot switches

--- run/run_ISS_0.2.0.R
+++ run/run_ISS_0.2.0.R
@@ -1,9 +1,11 @@
-# First try with Steve's y2 object prepped with trip join and sampling strata
+# First try booting trip, haul, length with seperate switches.
 
-iters = 300
+iters = 3
 lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/baseISS_data/inputs/y2_nosex_ebs_pcod_Steve_TRIP_STRATA.RDS")
-yrs = NULL
-boot_thl = TRUE
+yrs = 2020
+boot.trip = TRUE # try a dot "." for switches when there is already exists a function with "_"
+boot.haul = TRUE
+boot.length = TRUE
 expand_by_sampling_strata = TRUE
 expand_using_weighting_factors = TRUE
 
@@ -12,7 +14,9 @@ tictoc::tic()
 fishery_iss(iters = iters,
             lfreq_data = lfreq_data,
             yrs = yrs,
-            boot_thl = boot_thl,
+            boot.trip = boot.trip,
+            boot.haul = boot.haul,
+            boot.length = boot.length,
             expand_by_sampling_strata = expand_by_sampling_strata,
             expand_using_weighting_factors = expand_using_weighting_factors) -> .output
 tictoc::toc()
