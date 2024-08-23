88ad09c5d7d9c562b0d0352ee91ea97021626169
Brett Stacy <bstacy2@uw.edu>
2024-08-23 10:23:43 -0700

    sex post-strata works. still need to consider if the expansion code should be sex-specific as it is in the EBS pcod expansion scripts.

---
+++ run/run_ISS_0.2.4.R
@@ -0,0 +1,32 @@
+# Post_stratify take 3. Try stratifying sex
+run_name = "post_stratify_sex"
+iters = 2
+lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/baseISS_data/inputs/y2_sex_ebs_pcod_Steve_TRIP_STRATA.RDS")
+library(tidytable)
+
+yrs = NULL
+post_strata = list(strata = c("SEX")) # try SEX post-strata - works
+boot.trip = T
+boot.haul = T
+boot.length = T
+expand.by.sampling.strata = TRUE
+expand_using_weighting_factors = TRUE
+
+
+tictoc::tic()
+fishery_iss(iters = iters,
+            lfreq_data = lfreq_data,
+            yrs = yrs,
+            post_strata = post_strata,
+            boot.trip = boot.trip,
+            boot.haul = boot.haul,
+            boot.length = boot.length,
+            expand.by.sampling.strata = expand.by.sampling.strata,
+            expand_using_weighting_factors = expand_using_weighting_factors) -> .output
+tictoc::toc()
+.output
+
+
+saveRDS(.output, file = paste0("C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/baseISS_data/outputs/", run_name, "_ISS_", iters, "iters_output_", Sys.Date(), ".RDS"))
+
+
