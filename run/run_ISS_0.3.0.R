ba29c0d93854d745fd0c6de7fc494d02c90a7e96
Brett Stacy <bstacy2@uw.edu>
2024-08-28 08:52:51 -0700

    Initiate age functionality

---
+++ run/run_ISS_0.3.0.R
@@ -0,0 +1,33 @@
+# Age
+run_name = "age_V1"
+iters = 2
+lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/baseISS_data/inputs/y2_age_sex_ebs_pcod_Steve_TRIP_STRATA.RDS")
+library(tidytable)
+
+yrs = NULL
+post_strata = list(strata = c("SEX"))
+boot.trip = T
+boot.haul = T
+boot.length = T
+boot.age = T
+expand.by.sampling.strata = TRUE
+expand_using_weighting_factors = TRUE
+
+
+tictoc::tic()
+fishery_iss(iters = iters,
+            lfreq_data = lfreq_data,
+            yrs = yrs,
+            post_strata = post_strata,
+            boot.trip = boot.trip,
+            boot.haul = boot.haul,
+            boot.length = boot.length,
+            expand.by.sampling.strata = expand.by.sampling.strata,
+            expand_using_weighting_factors = expand_using_weighting_factors) -> .output
+tictoc::toc()
+.output
+
+
+saveRDS(.output, file = paste0("C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/baseISS_data/outputs/", run_name, "_ISS_", iters, "iters_output_", Sys.Date(), ".RDS"))
+
+
