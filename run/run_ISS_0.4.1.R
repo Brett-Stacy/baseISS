8f4618795ee248429171b41531f5ac69d4822749
Brett Stacy <bstacy2@uw.edu>
2024-09-13 09:26:24 -0700

    test speed ~3 seconds per iteration without post-strata.

---
+++ run/run_ISS_0.4.1.R
@@ -0,0 +1,45 @@
+# Length - speed test without strata
+run_name = "length_uncount_V2"
+
+lfreq_data = readRDS(file = "C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/baseISS_data/inputs/y2_sex_ebs_pcod_Steve_TRIP_STRATA.RDS")
+library(tidytable)
+library(baseISS)
+
+
+species_code = "202"
+area_code = "EBS"
+length_based = TRUE # TRUE for length
+iters = 10
+freq_data = lfreq_data
+yrs = NULL
+post_strata = NULL
+minimum_sample_size = NULL
+boot.trip = T
+boot.haul = T
+boot.length = T
+boot.age = F
+expand.by.sampling.strata = TRUE
+expand_using_weighting_factors = TRUE
+
+
+tictoc::tic()
+fishery_iss(species_code = species_code,
+            area_code = area_code,
+            length_based = length_based,
+            iters = iters,
+            freq_data = freq_data,
+            yrs = yrs,
+            post_strata = post_strata,
+            boot.trip = boot.trip,
+            boot.haul = boot.haul,
+            boot.length = boot.length,
+            boot.age = boot.age,
+            expand.by.sampling.strata = expand.by.sampling.strata,
+            expand_using_weighting_factors = expand_using_weighting_factors) -> .output
+tictoc::toc()
+.output
+
+
+saveRDS(.output, file = paste0("C:/Users/bstacy2/OneDrive - UW/UW Postdoc/GitHub Repos/baseISS_data/outputs/", run_name, "_ISS_", iters, "iters_output_", Sys.Date(), ".RDS"))
+
+
