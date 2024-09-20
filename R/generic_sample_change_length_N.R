633114f625d7d2dcabf84f7944ab87dfa5cf426a
Brett Stacy <bstacy2@uw.edu>
2024-09-20 09:50:43 -0700

    add function to sample flexible N.

---
+++ R/generic_sample_change_length_N.R
@@ -0,0 +1,23 @@
+#' Resample with replacement a vector of user-defined length. More generic than simply using base::sample
+#'
+#' @description
+#' Modified generic_sample() function to allow more flexibility based on user-defined options for adjusting haul-level sample size.
+#'
+#' @param flex.vec flexible (can be length 1) vector of input elements
+#' @param new_length_N user-defined sample size adjustment options defined in fishery_iss()
+#'
+#' @return vector of resampled elements
+#'
+#' @export
+#'
+generic_sample_change_length_N = function(flex.vec,
+                                          new_length_N = NULL)
+  {
+  # 0.5.0 case
+  flex.vec[base::sample.int(length(flex.vec), n = min(.N, new_length_N$amount), replace = TRUE)]
+
+}
+
+
+
+
