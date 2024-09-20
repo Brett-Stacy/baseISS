0c67c94e90fcfd42d671c19ce886a8c21d1aeb39
Brett Stacy <bstacy2@uw.edu>
2024-09-20 09:14:25 -0700

    add alternative sample function to boot_length when new_length_N is not NULL

--- R/boot_length.R
+++ R/boot_length.R
@@ -10,7 +10,8 @@
 boot_length = function(length_DT,
                        new_length_N = NULL) {
   length_DT %>%
-    tidytable::mutate(LENGTH = generic_sample(LENGTH, n.samples = ifelse(base::is.null(new_length_N), .N, min(.N, new_length_N$amount))), .by = c(YEAR, HAUL_JOIN)) %>%
+    tidytable::mutate(LENGTH = ifelse(base::is.null(new_length_N), generic_sample(LENGTH, n.samples = .N), generic_sample_change_length_N(LENGTH, new_length_N)), .by = c(YEAR, HAUL_JOIN)) %>%
+    # tidytable::mutate(LENGTH = generic_sample(LENGTH, n.samples = ifelse(base::is.null(new_length_N), .N, min(.N, new_length_N$amount))), .by = c(YEAR, HAUL_JOIN)) %>%
     tidytable::summarise(SUM_FREQUENCY = n(base::unique(LENGTH)), .by = c(YEAR, HAUL_JOIN, LENGTH)) %>% # after this line is where I would incorporate a different sample size feature. would need to set YAGMH_SFREQ to requested sample size.
     tidytable::mutate(YAGMH_SFREQ = base::sum(SUM_FREQUENCY), .by = c(YEAR, HAUL_JOIN)) %>%
     tidytable::left_join(length_DT %>%
