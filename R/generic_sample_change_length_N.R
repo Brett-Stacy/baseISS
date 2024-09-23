24cb052ba0ed40d250d0efd882d49982a402fca0
Brett Stacy <bstacy2@uw.edu>
2024-09-23 15:16:18 -0700

    adjust length N sample function to accept more custom options. tested - works.

--- R/generic_sample_change_length_N.R
+++ R/generic_sample_change_length_N.R
@@ -13,10 +13,24 @@
 generic_sample_change_length_N = function(flex.vec,
                                           new_length_N = NULL)
   {
-  # 0.5.0 case
-  flex.vec[base::sample.int(length(flex.vec), size = min(length(flex.vec), new_length_N$amount), replace = TRUE)]
 
-}
+  # value
+  if(new_length_N$type=="value"){
+    flex.vec[base::sample.int(length(flex.vec),
+                              size = ifelse(is.null(new_length_N$bound), new_length_N$amount,
+                                            ifelse(new_length_N$bound=="minimum", min(length(flex.vec), new_length_N$amount),
+                                                   max(length(flex.vec), new_length_N$amount))),
+                              replace = TRUE)] -> .new_sample
+  }
+
+  # proportion
+  if(new_length_N$type=="proportion"){
+    stop("Under development. Proportion of what? annual median sample size? median sample size over the yrs? how would I calculate that in this function? I would have to calculate it higher up with the length_DT object. also, should there be an option for median or mean? and how should I round? to the whole number?")
+  }
+
+  return(.new_sample)
+
+  }
 
 
 
