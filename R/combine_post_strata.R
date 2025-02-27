2943d43a974a20264607247e2ce61ff7392f6745
Brett Stacy <bstacy2@uw.edu>
2025-02-27 10:55:27 -0800

    continue developing combine_post_strata functionality

---
+++ R/combine_post_strata.R
@@ -0,0 +1,14 @@
+#' Combine post-stratified ISS results
+#'
+#' @description
+#' Combine the post-stratified ISS results by post-strata fish weight or numbers as weighted average. Includes evidence somewhere (DECIDE BEST PLACE) that states it was combined by post_strata and reports what the strata were and the method used to combine them.
+#'
+#' @param out_stats from fishery_iss()
+#'
+#' @return out_stats object to be returned by fishery_iss() that includes one vector of ISS based on the weighted average between post-strata
+#'
+#' @export
+#'
+combine_post_strata = function(out_stats){
+
+}
